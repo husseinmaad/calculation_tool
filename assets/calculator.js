1c6a4bdeb4d2fd4b5ec5403da89cae7dd2c94350
husseinmaad <hussein.maad@outlook.com>
2017-07-16 17:16:25 -0700

    add calculate method

--- assets/calculator.js
+++ assets/calculator.js
@@ -13,7 +13,13 @@
     $scope.hal_conversion = .0450;
     $scope.inc_conversion = .0146;
     $scope.inc_conversion = .0125;
-   
+    // to calculate the wattage we will multiply
+    // the number of watt with conversation number 
+    $scope.calculate = function(){
+      $scope.inc_wattage = ($scope.current_lumens * $scope.inc_conversion).toFixed(1);
+      console.log($scope.inc_wattage )
+    }
+    $scope.calculate();
   }]);
 })()
 
